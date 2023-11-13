425c92485f2c1b6707d0742f5d3a587d3acd2b21
kartikeya.sharma@srh-heidelberg.org <kartikeya.sharma@srh-heidelberg.org>
2023-11-13 23:10:26 +0000

    im1

---
+++ models/gcp_fetch_nextbike_data/Inventory_Management.sql
@@ -0,0 +1,16 @@
+WITH filtered_bike_data AS (
+    SELECT
+        b.station_name,
+        b.available_bikes,
+        b.bike_racks,
+        w.prcp
+    FROM
+        `nextbike-404713.nextbike.Live3` b
+    JOIN
+        `nextbike-404713.nextbike.Weather_Data` w ON b.city_name = w.City_Name
+    WHERE
+        b.available_bikes < b.set_point_bikes AND w.prcp = 0
+)
+
+SELECT *
+FROM filtered_bike_data
