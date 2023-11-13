4dbce948df75aff1c232c4bff77960e04127440a
kartikeya.sharma@srh-heidelberg.org <kartikeya.sharma@srh-heidelberg.org>
2023-11-13 23:05:25 +0000

    dp7

--- models/gcp_fetch_nextbike_data/dynamic_pricing.sql
+++ models/gcp_fetch_nextbike_data/dynamic_pricing.sql
@@ -1,22 +1,22 @@
 WITH CombinedData AS (
   SELECT
     b.*,
-    w.weather_description
+    CASE
+    WHEN w.prcp <= 0.1 THEN 'Sunny'
+    WHEN w.prcp > 0.1 AND w.prcp <= 1.0 THEN 'Light Rain'
+    WHEN w.prcp > 1.0 AND w.prcp <= 2.0 THEN 'Moderate Rain'
+    WHEN w.prcp > 2.0 AND w.prcp <= 3.0 THEN 'Heavy Rain'
+    WHEN w.prcp > 3.0 AND w.prcp <= 4.0 THEN 'Storm'
+    WHEN w.prcp > 4.0 AND w.prcp <= 5.0 THEN 'Snowy'
+    ELSE 'Unknown'
+  END AS weather_description
   FROM
     `nextbike-404713.nextbike.Live3` b
   JOIN
     `nextbike-404713.nextbike.Weather_Data` w ON b.city_name = w.City_Name
 )
 SELECT
-    CASE
-    WHEN prcp <= 0.1 THEN 'Sunny'
-    WHEN prcp > 0.1 AND prcp <= 1.0 THEN 'Light Rain'
-    WHEN prcp > 1.0 AND prcp <= 2.0 THEN 'Moderate Rain'
-    WHEN prcp > 2.0 AND prcp <= 3.0 THEN 'Heavy Rain'
-    WHEN prcp > 3.0 AND prcp <= 4.0 THEN 'Storm'
-    WHEN prcp > 4.0 AND prcp <= 5.0 THEN 'Snowy'
-    ELSE 'Unknown'
-  END AS weather_description ,
+  weather_description ,
   AVG(booked_bikes) AS avg_booked_bikes,
   CASE
     WHEN AVG(booked_bikes) > 50 THEN 'High Demand - Increase Price'
