b4b60fcdeede325dfcc0c656be5dd1dd8cf01b5c
kartikeya.sharma@srh-heidelberg.org <kartikeya.sharma@srh-heidelberg.org>
2023-11-13 23:22:06 +0000

    im7

--- models/gcp_fetch_nextbike_data/Inventory_Management.sql
+++ models/gcp_fetch_nextbike_data/Inventory_Management.sql
@@ -7,8 +7,10 @@ WITH filtered_bike_data AS (
     FROM
         `nextbike-404713.nextbike.Live3` b
     JOIN
-        `nextbike-404713.nextbike.Weather_Data` w ON b.city_name = w.City_name
+        `nextbike-404713.nextbike.Weather_Data` w ON LOWER(b.city_name) = LOWER(w.City_Name)
+    WHERE
+        b.available_bikes < b.set_point_bikes AND w.prcp = 0
 )
 
 SELECT *
-FROM filtered_bike_data
+FROM filtered_bike_data;
