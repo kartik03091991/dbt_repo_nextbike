14e93d583e01a1b1f2b1884bd2e7cf314a66cc1e
kartikeya.sharma@srh-heidelberg.org <kartikeya.sharma@srh-heidelberg.org>
2023-11-14 22:22:14 +0000

    ms

--- models/gcp_fetch_nextbike_data/Maintenence_Scheduling.sql
+++ models/gcp_fetch_nextbike_data/Maintenence_Scheduling.sql
@@ -1,8 +1,7 @@
 WITH filtered_bike_data AS (
     SELECT
-        b.city_name,
-        b.station_name,
-        b.booked_bikes,
+        DISTINCT b.city_name,
+        b.city_booked_bikes,
         w.rhum
     FROM
         `nextbike-404713.nextbike.Live3` b
@@ -14,10 +13,10 @@ WITH filtered_bike_data AS (
 
 SELECT
     city_name,
-    station_name,
-    SUM(booked_bikes) as total_booked,
+    SUM(city_booked_bikes) as total_booked,
     AVG(rhum) as average_humidity
 FROM
     filtered_bike_data
 GROUP BY
-    city_name, station_name
+    city_name
+ORDER BY average_humidity DESC
