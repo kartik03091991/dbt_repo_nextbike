80c6a277fdf01f350291a64a42ae7c67c707c43a
kartikeya.sharma@srh-heidelberg.org <kartikeya.sharma@srh-heidelberg.org>
2023-11-14 22:29:53 +0000

    wqw

--- models/gcp_fetch_nextbike_data/Optimal_Bike_Rack_Allocation.sql
+++ models/gcp_fetch_nextbike_data/Optimal_Bike_Rack_Allocation.sql
@@ -1,8 +1,14 @@
+-- This is a dbt model that aggregates data from bike_data and weather_data
+
 WITH filtered_bike_data AS (
     SELECT
+        b.city_name,
         b.station_name,
         AVG(b.station_bikes) as avg_available_bikes,
-        AVG(b.station_bikes - b.bike_racks ) as avg_rack_shortage
+        AVG(b.station_bikes - b.bike_racks) as avg_rack_shortage,
+        AVG(w.temp) as avg_temperature,
+        AVG(w.prcp) as avg_precipitation,
+        AVG(w.rhum) as avg_humidity
     FROM
         `nextbike-404713.nextbike.Live3` b
     JOIN
@@ -10,17 +16,20 @@ WITH filtered_bike_data AS (
     WHERE
         w.temp > 5 AND w.prcp = 0
     GROUP BY
-        b.station_name
-
+        b.city_name, b.station_name
 )
 
 SELECT
+    city_name,
     station_name,
     avg_available_bikes,
-    avg_rack_shortage
+    avg_rack_shortage,
+    avg_temperature,
+    avg_precipitation,
+    avg_humidity
 FROM
     filtered_bike_data
 WHERE 
     avg_rack_shortage > 5
-ORDER BY avg_rack_shortage DESC
-
+ORDER BY
+    avg_rack_shortage DESC
